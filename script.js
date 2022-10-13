5a3ec414c154bb4d16a724494ab6720a08493653
joykomaling <joyfernin11@gmail.com>
2022-10-13 11:03:59 +0700

    community card update

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.nav-links ul');
 
-menuToggle.addEventListener('click', function() {
+menuToggle.addEventListener('click', function () {
     nav.classList.toggle('slide')
 })
+
+const data = [
+    {
+        image: "link",
+        text: "lorem ipsum"
+    },
+    {
+        image: "link",
+        text: "lorem ipsum"
+    },
+    {
+        image: "link",
+        text: "lorem ipsum"
+    }
+]
